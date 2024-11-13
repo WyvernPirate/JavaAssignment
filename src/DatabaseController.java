bda3ebdc325f43236a1747cc7071908f5ae9b7ca
Phemelo Moloi <moloieric80@gmail.com>
2024-11-13 21:54:18 +0200

    "Updated GUI class to add functionality for admin and student login, registration, and frames; modified Student class to include username, password, and date of birth fields."

---
+++ src/DatabaseController.java
@@ -0,0 +1,52 @@
+import java.sql.*;
+
+public class DatabaseController {
+
+    // instance variables
+    private Connection conn = null;
+    private String url = "jdbc:mysql://localhost:3306/student";
+
+    // method to connect to the database
+    public boolean connect() {
+        boolean flag = false;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // establish the connection
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection successful");
+
+            flag = true;
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found.");
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database: ");
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    // create a new student account
+    public boolean createStudentAccounts(Student p) {
+        boolean flag = false;
+        try {
+            String query = "INSERT INTO Student(StudentID, Name, Surname, Program, Year, Dob) VALUES(?,?,?,?,?,?)";
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, p.getStudentId());
+            pstmt.setString(2, p.getFirstName());
+            pstmt.setString(3, p.getLastName());
+            pstmt.setString(4, p.getProgram());
+            pstmt.setInt(5, p.getYear());
+            pstmt.setDate(6, p.getDob());
+            pstmt.executeUpdate();
+            flag = true;
+        } catch (SQLException e) {
+            System.out.println("Error creating student account: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+        return flag;
+    }
+
+}
